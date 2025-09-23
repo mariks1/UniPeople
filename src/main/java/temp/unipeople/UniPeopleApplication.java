0f2cd481172d8b3d632746a36b6ba2441e97da88
mariks1 <marat-khasanshin@list.ru>
2025-09-23 22:26:15 +0300

    Initial commit

---
+++ src/main/java/temp/unipeople/UniPeopleApplication.java
@@ -0,0 +1,13 @@
+package temp.unipeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniPeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniPeopleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/temp/unipeople/UniPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package temp.unipeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniPeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
