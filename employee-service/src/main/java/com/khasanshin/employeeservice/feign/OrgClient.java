b5a72a03fb786171c8c231a10446d8e97ffac5cc
mariks1 <marat-khasanshin@list.ru>
2025-11-27 23:35:22 +0300

    feat: feighAuth in orgClients

--- employee-service/src/main/java/com/khasanshin/employeeservice/feign/OrgClient.java
+++ employee-service/src/main/java/com/khasanshin/employeeservice/feign/OrgClient.java
@@ -1,13 +1,14 @@
 package com.khasanshin.employeeservice.feign;
 
 
+import com.khasanshin.employeeservice.config.FeignAuthConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@FeignClient(name = "organization-service", path = "/api/v1/departments")
+@FeignClient(name = "organization-service", path = "/api/v1/departments", configuration = FeignAuthConfig.class)
 public interface OrgClient {
 
     @RequestMapping(method = RequestMethod.HEAD, value = "/{id}")
