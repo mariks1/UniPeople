c05cc0a98d448cf3230ff5ee047b9a3c640b6ce8
mariks1 <marat-khasanshin@list.ru>
2026-01-19 12:31:40 +0300

    tests: e2e tests with kafka/s3

--- file-service/src/main/java/com/khasanshin/fileservice/config/SecurityConfig.java
+++ file-service/src/main/java/com/khasanshin/fileservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.khasanshin.fileservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
+@Profile("!test")
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
 
